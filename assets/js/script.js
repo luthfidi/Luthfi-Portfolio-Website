430016f315b5e6f14a8ca4f10d4e1a4d4ca7a361
Luthfi <luthfihadi543@gmail.com>
2024-10-05 18:36:30 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -287,24 +287,24 @@ document.addEventListener('mousemove', onMouseMove, false);
 initCursor();
 updateCursor();
 
-document.addEventListener('DOMContentLoaded', function() {
-  const images = document.querySelectorAll('img');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const images = document.querySelectorAll('img');
   
-  images.forEach(img => {
-      img.addEventListener('dragstart', function(e) {
-          e.preventDefault();
-      });
+//   images.forEach(img => {
+//       img.addEventListener('dragstart', function(e) {
+//           e.preventDefault();
+//       });
       
-      img.addEventListener('contextmenu', function(e) {
-          e.preventDefault();
-      });
+//       img.addEventListener('contextmenu', function(e) {
+//           e.preventDefault();
+//       });
       
-      img.addEventListener('touchstart', function(e) {
-          e.preventDefault();
-      }, { passive: false });
+//       img.addEventListener('touchstart', function(e) {
+//           e.preventDefault();
+//       }, { passive: false });
       
-      img.addEventListener('touchmove', function(e) {
-          e.preventDefault();
-      }, { passive: false });
-  });
-});
+//       img.addEventListener('touchmove', function(e) {
+//           e.preventDefault();
+//       }, { passive: false });
+//   });
+// });
