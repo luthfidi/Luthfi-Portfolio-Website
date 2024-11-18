58b00191d096f42bc15d7a9ca9eb11302265d6da
Luthfi <luthfihadi543@gmail.com>
2024-11-19 05:02:58 +0700

    feat: dragstart image

--- assets/js/script.js
+++ assets/js/script.js
@@ -372,3 +372,13 @@ function initCustomScrollbar() {
 }
 
 initCustomScrollbar();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('img');
+    
+    images.forEach(img => {
+        img.addEventListener('dragstart', function(e) {
+            e.preventDefault();
+        });
+    });
+});
